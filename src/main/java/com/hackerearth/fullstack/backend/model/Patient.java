3ea3d9baa7e8e08442ff54ab52705b11b9fbe6f3
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-08-29 15:53:18 +0530

    Update Patient.java

--- src/main/java/com/hackerearth/fullstack/backend/model/Patient.java
+++ src/main/java/com/hackerearth/fullstack/backend/model/Patient.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Table;
 @Table(name = "patients")
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private int age;
