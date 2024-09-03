7a43d797e10b6a01aa454ecccfcb07a1677c8180
nsrikanthreddy <nallaballesrikanthreddy@gmail.com>
2024-09-03 14:38:36 +0530

    Update PatientController.java

--- src/main/java/com/hackerearth/fullstack/backend/controller/PatientController.java
+++ src/main/java/com/hackerearth/fullstack/backend/controller/PatientController.java
@@ -147,9 +147,9 @@ public class PatientController {
     }
 
     @GetMapping("/doctor/{doctorName}")
-    public ResponseEntity<List<Patient>> getPatientsByDoctorName(@PathVariable String doctorName, HttpServletRequest request) {
+    public ResponseEntity<List<Patient>> getPatientsByDoctorName(@PathVariable String doctorName) {
 	try {
-		String baseUrl = ServletUriComponentsBuilder.fromRequestUri(request).replacePath(null).build().toUriString();
+		String baseUrl = ServletUriComponentsBuilder.fromRequestUri(HttpServletRequest request).replacePath(null).build().toUriString();
         String SystemName = InetAddress.getLocalHost().getHostName();
 		logger.info("in getPatientsByDoctorName & System Name : ------>"+ SystemName);
 		logger.info("in getPatientsByDoctorName & baseUrl : ------>"+ baseUrl);
